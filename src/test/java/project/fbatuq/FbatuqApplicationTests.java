4cfb129f86fdd9116979ccf17900666db61f8385
iwtd-code <iwtd-code@o2.pl>
2020-11-21 13:49:25 +0100

    start

---
+++ src/test/java/project/fbatuq/FbatuqApplicationTests.java
@@ -0,0 +1,13 @@
+package project.fbatuq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbatuqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

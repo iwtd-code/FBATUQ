b5a42120cecca4e19e2aaa097eeb1f1d0b326d73
iwtd-code <iwtd-code@o2.pl>
2020-11-22 11:26:48 +0100

    addmessage commit

--- src/main/java/project/fbatuq/controller/MessageController.java
+++ src/main/java/project/fbatuq/controller/MessageController.java
@@ -1,4 +1,28 @@
 package project.fbatuq.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import project.fbatuq.model.dto.MessageDTO;
+import project.fbatuq.service.MessageService;
+
+
+@Controller
 public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/addmsg")
+    public ModelAndView getAddMessageView() {
+        return new ModelAndView("addmsg", "newMessage", new MessageDTO());
+    }
+
+
 }
--- src/main/java/project/fbatuq/service/MessageService.java
+++ src/main/java/project/fbatuq/service/MessageService.java
@@ -1,4 +1,35 @@
 package project.fbatuq.service;
 
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import project.fbatuq.model.dto.MessageDTO;
+import project.fbatuq.model.entity.Message;
+import project.fbatuq.repository.MessageRepository;
+
+@Service
 public class MessageService {
+
+    private final ModelMapper modelMapper;
+
+    private MessageRepository repository;
+
+    public MessageService(ModelMapper modelMapper, MessageRepository repository) {
+        this.modelMapper = modelMapper;
+        this.repository = repository;
+    }
+
+    public ModelMapper getModelMapper() {
+        return modelMapper;
+    }
+
+    public void addMessage(MessageDTO messageDTO){
+        Message message = modelMapper.map(messageDTO, Message.class);
+
+        System.out.println("New message: " + message.getText());
+
+        repository.save(message);
+    }
+
+
 }

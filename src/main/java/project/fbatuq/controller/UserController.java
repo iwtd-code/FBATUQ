ec0ee2bbfb3746f990f6fff977f01f03f3f5c5e9
Dimon_Keval <kevaldima@gmail.com>
2020-11-22 10:36:50 +0100

    adduser.html commit

--- src/main/java/project/fbatuq/controller/UserController.java
+++ src/main/java/project/fbatuq/controller/UserController.java
@@ -1,9 +1,11 @@
 package project.fbatuq.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import project.fbatuq.model.dto.UserDTO;
 
+@Controller
 public class UserController {
 
     @GetMapping("/adduser")

8b48bd33ce965e6530fab5d4dbc12cf4839a4076
Dimon_Keval <kevaldima@gmail.com>
2020-11-22 11:40:04 +0100

    UserController has to new methods for Get i Post on "/adduser"

--- src/main/java/project/fbatuq/controller/UserController.java
+++ src/main/java/project/fbatuq/controller/UserController.java
@@ -2,14 +2,28 @@ package project.fbatuq.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import project.fbatuq.model.dto.UserDTO;
+import project.fbatuq.service.UserService;
 
 @Controller
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/adduser")
     public ModelAndView getAddNewUserData () {
         return new ModelAndView("adduser", "newUser", new UserDTO());
+    }
+
+    @PostMapping("/adduser")
+    public String addNewUserAccount (@ModelAttribute UserDTO userDTO) {
+        userService.addNewUser(userDTO);
+        return "index";
     }
 }

56a01b702590d8769232b9cbb6847e4fa70054ff
iwtd-code <iwtd-code@o2.pl>
2020-11-29 10:00:45 +0100

    added method in IndexController

--- src/main/java/project/fbatuq/controller/IndexController.java
+++ src/main/java/project/fbatuq/controller/IndexController.java
@@ -16,4 +16,9 @@ public class IndexController {
     public String login(){
         return "login";
     }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
 }

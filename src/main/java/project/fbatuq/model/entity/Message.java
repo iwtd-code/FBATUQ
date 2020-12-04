547bbb7094f4d1c8fd8be5f810863638b12f5ac0
iwtd-code <iwtd-code@o2.pl>
2020-12-04 11:53:30 +0100

    Message added getter, setter id

--- src/main/java/project/fbatuq/model/entity/Message.java
+++ src/main/java/project/fbatuq/model/entity/Message.java
@@ -9,9 +9,7 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String text;
-
     private Timestamp timestamp;
 
     public Message() {
@@ -23,8 +21,16 @@ public class Message {
         this.timestamp = timestamp;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getText() {
         return text;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setText(String text) {
